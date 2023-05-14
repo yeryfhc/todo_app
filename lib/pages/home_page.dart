8f49d419f5ad38707c7ae763b3dbbd3ad96a8c39
Yerytech <yeryfhilario@gmail.com>
2023-05-13 18:08:29 -0700

    change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 
+
 import 'package:flutter/material.dart';
 
 import '../widgets/widgets.dart';
@@ -20,23 +21,64 @@ const HomePage({Key? key}) : super(key: key);
         
         backgroundColor: const Color.fromARGB(79, 158, 158, 158),
       ),
-      body:   SingleChildScrollView(
-        child: 
-          ListView.builder(
-            
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return  const TheCardNote() ;
-            },
-          ),
-        
+      body:   SizedBox(
+        height: 800,
+        child: ListView.builder(
+          
+          itemCount: 15,
+          itemBuilder: (BuildContext context, int index) {
+            return  const TheCardNote() ;
+          },
+        ),
       ), floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.ad_units),
         onPressed: (){
-         
+         showDialog(
+        
+          barrierColor: Colors.transparent,
+          context: context, builder: (context) {
+          return const  AddNote();
 
+         } );
         }),
    );
+  }
+}
+
+class AddNote extends StatelessWidget {
+  const AddNote({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Stack(
+    
+         children: [
+     Center(
+       child: SizedBox(
+        width: double.infinity,
+        height: 300,
+         child: Card(
+          margin: EdgeInsets.all(10),
+          color: Color.fromARGB(220, 246, 255, 0),
+           child: Column(children: [
+             Text('Add Note',style: TextStyle(color: Colors.deepPurple,fontSize: 30,fontWeight: FontWeight.bold),),
+            TextField(
+            decoration: InputDecoration(
+                hintText: 'Title',
+              disabledBorder: InputBorder.none)
+
+            )
+           
+           ],),
+         ),
+       ),
+     )
+
+         ],
+  
+    );
   }
 }
 
