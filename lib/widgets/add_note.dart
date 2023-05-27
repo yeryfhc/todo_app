7cc83edbbb094ee013fe6ab5970b83c12f882abb
Yerytech <yeryfhilario@gmail.com>
2023-05-26 19:42:56 -0700

    update

--- lib/widgets/add_note.dart
+++ lib/widgets/add_note.dart
@@ -9,11 +9,28 @@ class AddNote extends StatelessWidget {
   Widget build(BuildContext context) {
      bool check= false;
     return   Dialog(
-      backgroundColor:   Color.fromARGB(255, 255, 145, 0),
+      backgroundColor: const   Color.fromARGB(255, 255, 145, 0),
       child: Column(children:  [
        const  Center(child: Text('Add Note',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),),
       const Center(child: TextField()),
-       Checkbox(value:check , onChanged:(bool? check){} ),
+       Row( 
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+         children: [
+          const Text('Priority',style: TextStyle(fontSize: 20),),
+           Checkbox(value:check , onChanged:(bool? check){} ),
+         ],
+       ),
+      const  SizedBox(height: 50,),
+       Row( 
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+         children: [
+           TextButton(onPressed: (){},
+            style: const ButtonStyle(backgroundColor:MaterialStatePropertyAll(Colors.yellow) ), child: const  Text('Exit',style: TextStyle(color: Colors.white,fontSize: 25),),
+           ),
+           TextButton(onPressed: (){}, child: const  Text('Add',style: TextStyle(color: Colors.white,fontSize: 25))),
+
+         ],
+       ),
 
 
       ],),
